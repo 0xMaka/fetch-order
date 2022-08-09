3c600ec03c37c8b6cee8bf3d5bce88682e9898d9
Maka Gucci <makagucci@gmx.com>
2022-08-09 22:05:23 +0100

    Legacy and housekeeping

--- fetch_order.py
+++ fetch_order.py
@@ -16,7 +16,10 @@ def get_value(_orders, _key):
 
 def _store(_json):
   data, pending_orders, other_orders = _sort(_json)
-  maker = get_value(other_orders, 'maker')
+  try:
+    maker = get_value(other_orders, 'maker')
+  except:
+    maker = get_value(pending_orders, 'maker')
   with open(f'logs/{maker}.log', 'w') as f:
     f.write(json.dumps(_json, indent=2))
     f.close
@@ -38,31 +41,36 @@ def store_limit_order(_address, _chainId):
 def fetch_and_index(_address, _chainId):
   j = _order(_address, _chainId)
   data, pending_orders, other_orders = _sort(j)
-  
+  total_pending = pending_orders['totalPendingOrders'] 
+  total_other = other_orders['totalOrders'] 
+
+  total = total_pending + total_other
+  if total == 0:
+    return [[0, 0, 0], 0, 0]
+
+  totals = [total, total_pending, total_other]
   pending = []
   other = []
 
-  total = other_orders['totalOrders']
-  total_pending = len(pending_orders['orders']) 
-  total_other = total - total_pending 
-
-  if total == 0:
-    print('No orders found')
-  elif total_pending and total_other > 0:
+  if total_pending and total_other > 0:
       for i in pending_orders:
         pending.append(pending_orders[i])
       for i in other_orders:
         other.append(other_orders[i])
-  else:
-    print('No pending orders found')
+      return [totals, pending[0], other[0]]
+  elif total_other > 0:
     for i in other_orders: other.append(other_orders[i])
-  
-  return [[total, total_pending, total_other], pending[0], other[0]]
+    return [totals, 0, other[0]] 
+  else:
+    for i in pending_orders: pending.append(pending_orders[i])
+    return [totals, pending[0], 0] 
 
 def main():
-  user = '0xB6207FaCFbc0C7373425D9671Ea0Ca23459E9796'
+  user0 = '0xB6207FaCFbc0C7373425D9671Ea0Ca23459E9796' 
+  user1 = '0x8c14ed4F602ac4d2Be8Ed9c4716307c73e9A83A8'
   chainId = 137
-  fetch_and_index(user, chainId)
+  print(fetch_and_index(user0, chainId))
+  store_limit_order(user1, chainId)
 
 if __name__ == '__main__':
   main()
